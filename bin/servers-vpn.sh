1e47645ef4fb430dc0686af373f45a60bbd90486
L3monBit <96775743+L3monBit@users.noreply.github.com>
2021-12-30 16:48:14 +0100

    Update servers-vpn.sh

--- bin/servers-vpn.sh
+++ bin/servers-vpn.sh
@@ -7,17 +7,17 @@ check_iface=$(ifconfig | grep -P "tun[\d?]")
 countries="$HOME/.config/bin/paises.txt"
 
 # Listando Menu y guardando el input
-option=$(/usr/bin/cat "$countries" |  rofi -i -dmenu -no-config -theme ~/.config/polybar/scripts/rofi/launcher.rasi -p "Nordvpn")
+option=$(/usr/bin/cat "$countries" |  rofi -i -dmenu -no-config -theme ~/.config/polybar/cuts/scripts/rofi/launcher.rasi -p "Nordvpn")
 
 if [[ -z "$check_iface" ]] ; then
- 	# Loop para comprobar si la option existe
-	while read -r line ; do
-		if [[ "$option" == "$line" ]] ; then
-			notify-send -u normal -t 1200 "En Proceso de Conexion : 🧙🏻‍♂"
-			/usr/bin/nordvpn c "$option"
-		fi
-	done < "$countries"
+  # Loop para comprobar si la option existe
+  while read -r line ; do
+    if [[ "$option" == "$line" ]] ; then
+      notify-send -u normal -t 1200 "En Proceso de Conexion : "
+      timeout 8 /usr/bin/nordvpn c "$option"
+    fi
+  done < "$countries"
 
 else
-	notify-send -u critical -t 1200 "Error: Ya existe una vpn conectada " 
+  notify-send -u critical -t 1200 "Error: Ya existe una vpn conectada " 
 fi
