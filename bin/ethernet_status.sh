feedc42e132e53f92620386852b724da06b2ee1a
L3monBit <96775743+L3monBit@users.noreply.github.com>
2021-12-30 16:47:35 +0100

    Update ethernet_status.sh

--- bin/ethernet_status.sh
+++ bin/ethernet_status.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 ethernet=$(/usr/sbin/ifconfig eth0 2>/dev/null| grep "inet " | awk '{print $2}')
 
-echo -e "%{F#00FFFF} %{F#ffffff} $ethernet%{u-}"
-
+echo -e "%{F#CCbd93f9} %{F#ffffff}$ethernet%{u-}"
