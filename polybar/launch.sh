19697ea2bc7c2627d46fd3987696f51dcd5f9190
L3monBit <96775743+L3monBit@users.noreply.github.com>
2021-12-30 17:52:14 +0100

    Update launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# Add this script to your wm startup file.
+DIR="$HOME/.config/polybar"
+
+# Terminate already running bar instances
 killall -q polybar
 
+# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar -q main -c config &
+# Launch the bar
+polybar -q bottom -c "$DIR"/config &
