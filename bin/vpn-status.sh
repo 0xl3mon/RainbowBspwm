eca48455a44caca32ae5a1836a7db0789a62a30f
L3monBit <96775743+L3monBit@users.noreply.github.com>
2021-12-30 17:36:32 +0100

    Update vpn-status.sh

--- bin/vpn-status.sh
+++ bin/vpn-status.sh
@@ -2,7 +2,7 @@
  vpn=$(/usr/sbin/ifconfig tun0 2>/dev/null | grep 'inet ' | awk '{print $2}')
  
  if [ -z "$vpn" ]; then
-     echo -e "%{F#CCc9c90a}  %{F#fff}Disconnected%{u-}"
+     echo "%{F#CCc9c90a}  %{F#fff}Disconnected%{u-}"
  elif [ -n "$vpn" ]; then
-     echo -e "%{F#CC50fa7b}  %{F#fff}Disconnected%{u-}"
+     echo "%{F#CC50fa7b}  %{F#fff}Disconnected%{u-}"
  fi
